a7df0439f31bedcc30006244e9df77870804dfd9
imvikaskashyap <mvikaskashyap@gmail.com>
2024-10-19 22:10:30 +0530

    flatlist

--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,7 @@
-import { View, Text, TextInput, Button, Alert, ScrollView } from "react-native";
-import React, { useState } from "react";
+import {  SafeAreaView } from "react-native";
+import React from "react";
+import { FlatList } from "react-native-web";
+import { Text } from "react-native";
 
 const Users = [
   { id: 1, name: "John" },
@@ -7,43 +9,17 @@ const Users = [
   { id: 3, name: "Jim" },
   { id: 4, name: "Jimmy" },
   { id: 5, name: "Jimola" },
-  { id: 1, name: "John" },
-  { id: 2, name: "Jane" },
-  { id: 3, name: "Jim" },
-  { id: 4, name: "Jimmy" },
-  { id: 5, name: "Jimola" },
-  { id: 1, name: "John" },
-  { id: 2, name: "Jane" },
-  { id: 3, name: "Jim" },
-  { id: 4, name: "Jimmy" },
-  { id: 5, name: "Jimola" },
-  { id: 2, name: "Jane" },
-  { id: 3, name: "Jim" },
-  { id: 4, name: "Jimmy" },
-  { id: 5, name: "Jimola" },
-  { id: 1, name: "John" },
-  { id: 2, name: "Jane" },
-  { id: 3, name: "Jim" },
-  { id: 4, name: "Jimmy" },
-  { id: 5, name: "Jimola" },
+ 
 ];
 
 const Index = () => {
   return (
-    <View>
-      <Text className="text-red-500 text-3xl mb-8 ">
-        Map the data in React Native
-      </Text>
-      <ScrollView>
-        <View className="flex flex-row w-full flex-wrap">
-      {Users.map((item) => (
-          <Text className="  w-20 h-20 p-4 m-4 border-2 border-black bg-orange-400">
-            {item.name}
-          </Text>
-      ))}
-        </View>
-        </ScrollView>
-    </View>
+    <SafeAreaView >
+     <FlatList
+        data={Users}
+        renderItem={({ item }) => <Text>{item.name}</Text>}
+      />
+    </SafeAreaView>
   );
 };
 
